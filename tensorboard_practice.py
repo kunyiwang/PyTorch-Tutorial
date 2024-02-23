f11ca3647d5d1692b238c9032af5cd7245c2c22c
kunyiW <kunyiwang252750@gmail.com>
2024-02-23 11:35:30 -0800

    Practice use transforms package to transform data

--- tensorboard_practice.py
+++ tensorboard_practice.py
@@ -1,10 +1,21 @@
 from torch.utils.tensorboard import SummaryWriter
+from PIL import Image
+import numpy as np
 
 if __name__ == '__main__':
     writer = SummaryWriter('logs')
 
     for i in range(100):
         writer.add_scalar('y=2x', 2*i, i)
+
+    img_path_val = 'data/val/ants/800px-Meat_eater_ant_qeen_excavating_hole.jpg'
+    img_path_train = 'data/train/ants/0013035.jpg'
+
+    img1 = np.array(Image.open(img_path_train))
+    img2 = np.array(Image.open(img_path_val))
+
+    writer.add_image('image', img1, 1, dataformats='HWC')
+    writer.add_image('image', img2, 2, dataformats='HWC')
 
     writer.close()
 
--- transform_data.py
+++ transform_data.py
@@ -8,8 +8,33 @@ if __name__ == '__main__':
     # img.show()
     writer = SummaryWriter('logs')
 
-    trans = transforms.ToTensor()
-    img_tensor = trans(img)
-
+    # ToTensor PIL -> Tensor
+    trans_toTensor = transforms.ToTensor()
+    img_tensor = trans_toTensor(img)
     writer.add_image('Tensor Image', img_tensor)
+
+    # Normalize PIL -> Tensor -> Normalize
+    trans_norm = transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    img_norm = trans_norm(img_tensor)
+    writer.add_image('Normalized Tensor Image', img_norm)
+
+    # Resize PIL -> Tensor -> Resize
+    # print(img_tensor.size())
+    trans_resize = transforms.Resize((512, 512))
+    img_resized = trans_resize(img_tensor)
+    writer.add_image('Resized Tensor Image', img_resized)
+    # print(img_resized.size())
+
+    # Compose PIL -> Tensor -> Normalize -> Resize
+    trans_compose = transforms.Compose([trans_toTensor, trans_norm, trans_resize])
+    img_composed = trans_compose(img)
+    writer.add_image('Composed Instructions to Image', img_composed)
+
+    # RandomCrop PIL -> Tensor -> Crop
+    trans_crop = transforms.RandomCrop((128,256))
+    trans_compose = transforms.Compose([trans_toTensor, trans_crop])
+    for i in range(10):
+        img_crop = trans_compose(img)
+        writer.add_image('Cropped Image', img_crop, i)
+
     writer.close()
